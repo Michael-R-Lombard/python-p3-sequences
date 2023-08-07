7fe54677953b7191090c0b58efc0407fdc064812
Michael Lombard <miklombard@yahoo.com>
2023-08-06 21:11:37 -0400

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_seq = []
+    if length > 0:
+        fib_seq.append(0)
+
+    for i in range(length - 1):  
+        if i == 0 and length > 1:
+            fib_seq.append(1)
+        else:
+            fib_seq.append(fib_seq[-1] + fib_seq[-2])
+
+    print(fib_seq)
